e9c69d93098c55e24002bf6e7a629c461ba10c98
Daniel Tse <daniel.tse1211@gmail.com>
2024-03-29 17:16:52 -0400

    Create category detail page

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,13 +2,31 @@ const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
 
 const Category = require('../models/category');
+const Item = require('../models/item');
 
 exports.categoryList = asyncHandler(async (req, res) => {
   const categories = await Category.find({}).exec();
   res.render('categoryList', { title: 'Category List', categories });
 });
 
-exports.categoryDetail = asyncHandler(async (req, res) => {});
+exports.categoryDetail = asyncHandler(async (req, res, next) => {
+  const [category, categoryItems] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }, { name: 1 }).exec(),
+  ]);
+
+  if (!category) {
+    const err = new Error('Category is not found.');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render('categoryDetail', {
+    title: category.name,
+    category,
+    categoryItems,
+  });
+});
 
 exports.categoryCreateGet = asyncHandler(async (req, res) => {});
 
