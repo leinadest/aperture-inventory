1b0748ea257fa0b1759011ae4270c8f237d9689b
Daniel Tse <daniel.tse1211@gmail.com>
2024-03-30 10:02:37 -0400

    Create category form page
     - Controller functionality and view have been implemented to load
       the category form page
     - Style.css is updated to stylize form control textarea across forms

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -28,9 +28,53 @@ exports.categoryDetail = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.categoryCreateGet = asyncHandler(async (req, res) => {});
+exports.categoryCreateGet = asyncHandler(async (req, res) => {
+  res.render('categoryForm', { title: 'Create category' });
+});
 
-exports.categoryCreatePost = asyncHandler(async (req, res) => {});
+exports.categoryCreatePost = [
+  // Sanitize and validate request data
+  body('name', 'Name must not be empty.').trim().notEmpty().escape(),
+  body('name', 'Name must be under 100 characters.')
+    .trim()
+    .isLength({ max: 100 })
+    .escape(),
+  body('description', 'Description must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  // Handle route
+  asyncHandler(async (req, res) => {
+    // Determine errors
+    const errorMsgs = validationResult(req)
+      .array()
+      .map((error) => error.msg);
+    const nameIsTaken =
+      (await Category.findOne({ name: req.body.name })) !== null;
+    if (nameIsTaken) errorMsgs.push('Name must be original.');
+
+    // Create sanitized category
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    });
+
+    // Handle errors
+    if (errorMsgs.length) {
+      res.render('categoryForm', {
+        title: 'Create category',
+        category,
+        errorMsgs,
+      });
+      return;
+    }
+
+    // Save category
+    await category.save();
+    res.redirect(category.url);
+  }),
+];
 
 exports.categoryDeleteGet = asyncHandler(async (req, res) => {});
 
