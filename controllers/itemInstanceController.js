c2498b950af1fe0396027f0bce488b65c7e8856b
Daniel Tse <daniel.tse1211@gmail.com>
2024-03-30 10:48:10 -0400

    Create item instance form page

--- controllers/itemInstanceController.js
+++ controllers/itemInstanceController.js
@@ -2,6 +2,7 @@ const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
 
 const ItemInstance = require('../models/itemInstance');
+const Item = require('../models/item');
 
 exports.itemInstanceList = asyncHandler(async (req, res) => {
   const itemInstances = await ItemInstance.find({}).populate('item').exec();
@@ -28,9 +29,51 @@ exports.itemInstanceDetail = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.itemInstanceCreateGet = asyncHandler(async (req, res) => {});
+exports.itemInstanceCreateGet = asyncHandler(async (req, res) => {
+  const allItems = await Item.find({}, 'name').sort({ name: 1 }).exec();
+  res.render('itemInstanceForm', { title: 'Create Item Instance', allItems });
+});
 
-exports.itemInstanceCreatePost = asyncHandler(async (req, res) => {});
+exports.itemInstanceCreatePost = [
+  // Sanitize and validate request data
+  body('item', 'Item must be specified.').trim().isLength({ min: 1 }).escape(),
+  body('status').escape(),
+  body('dueBack', 'Invalid date')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
+
+  // Handle route
+  asyncHandler(async (req, res) => {
+    // Determine errors
+    const errorMsgs = validationResult(req)
+      .array()
+      .map((error) => error.msg);
+
+    // Create sanitized item instance
+    const itemInstance = new ItemInstance({
+      item: req.body.item,
+      status: req.body.status,
+      dueBack: req.body.dueBack,
+    });
+
+    // Handle errors
+    if (errorMsgs.length) {
+      const allItems = await Item.find({}, 'name').sort({ name: 1 }).exec();
+      res.render('itemInstanceForm', {
+        title: 'Create Item Instance',
+        allItems,
+        itemInstance,
+        errorMsgs,
+      });
+      return;
+    }
+
+    // Save item instance
+    await itemInstance.save();
+    res.redirect(itemInstance.url);
+  }),
+];
 
 exports.itemInstanceDeleteGet = asyncHandler(async (req, res) => {});
 
